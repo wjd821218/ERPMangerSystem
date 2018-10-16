9c2eea0eefcafbda8b8b363dc8fbdd7efcdd0dce
王军栋 <215102284@qq.com>
2018-10-16 21:54:15 +0800

    2

--- ErpManage/ErpManage/frmLogin.designer.cs
+++ ErpManage/ErpManage/frmLogin.designer.cs
@@ -37,6 +37,7 @@
             this.lnkChangePassword = new System.Windows.Forms.LinkLabel();
             this.txtUserID = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -115,11 +116,21 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 153);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 62;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // frmLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(415, 292);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.txtUserID);
             this.Controls.Add(this.lnkChangePassword);
             this.Controls.Add(this.btnCancel);
@@ -150,5 +161,6 @@
         private System.Windows.Forms.LinkLabel lnkChangePassword;
         private System.Windows.Forms.TextBox txtUserID;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
